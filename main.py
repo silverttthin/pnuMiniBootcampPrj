922807f72f0a4721cd7f49b8a9114fe48677b561
chanho <thdcksgh15@gmail.com>
2025-02-09 13:05:31 +0900

    feat: home.html 작성 \ãn\n 홈화면에 어떤 ㅐ내용을 담을지 논의 필요

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+from fastapi import FastAPI, Request
+from pathlib import Path
+
 import controllers
 from controllers import UserController
 from db import create_db
 
 app = FastAPI()
 
+app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent / "static"),
+    name="static"
+)
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse(
+        'home.html', {"request": request})
+
 
 @app.on_event("startup")
 def on_startup():
     create_db()
-
 
 app.include_router(controllers.PlanController.router)
 app.include_router(UserController.router)
