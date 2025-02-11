6c6db109021502bb883ccd9c46f28e4bea51cb6b
Lee Si Woong <silverttthin@naver.com>
2025-02-11 11:51:25 +0900

    feat: 계획 페이지 라우팅에 토큰정보 추출해 전달 - 하드코딩된 html부분도 변경

--- routers/PlanController.py
+++ routers/PlanController.py
@@ -1,14 +1,16 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Body, Request
+from fastapi import APIRouter, Depends, Body, Request, HTTPException
 from fastapi.responses import HTMLResponse
 
 from dependencies.db import get_db_session
 from sqlalchemy.orm import Session
 from starlette.templating import Jinja2Templates
 
+from dependencies.jwt_utills import JWTUtil
 from requests.plan_schema import PlanCreate, PlanDeleteRequest, GetMyPlanListRequest
 from services.PlanService import PlanService
+from services.UserService import UserService
 
 router = APIRouter(prefix="/plans", tags=["Plan"])
 templates = Jinja2Templates(directory="templates")
@@ -17,8 +19,6 @@ templates = Jinja2Templates(directory="templates")
 # 세션을 주입받은 서비스 인스턴스 생성
 def get_plan_service(db: Session = Depends(get_db_session)) -> PlanService:
     return PlanService(db)
-
-# todo: 결과물을 CommonResponse에 담아야 함
 
 
 # 계획 생성
@@ -69,7 +69,7 @@ async def delete_plan(req: PlanDeleteRequest, planService: PlanService = Depends
 
 # 내가 작성한 게획 리스트 가져오기
 @router.post("/list")
-async def get_my_plans(req: GetMyPlanListRequest,
+async def get_my_plans(req: GetMyPlanListRequest, userService: UserService = Depends(),
                        planService: PlanService = Depends(get_plan_service)):
     plans = planService.get_my_plans(req)
     return plans
@@ -77,19 +77,19 @@ async def get_my_plans(req: GetMyPlanListRequest,
 
 # 플랜 작성 및 목록 페이지 라우팅
 @router.get("/page", response_class=HTMLResponse)
-async def read_plan_page(request: Request, planService: PlanService = Depends(get_plan_service)):
-    # todo: JWT token 받아 decoding 하기
-    user_id: int = 1
-    user_name: str = "이시웅"
-    plans = planService.get_my_plans(
-        GetMyPlanListRequest(user_id=user_id)
-    )
+async def read_plan_page(request: Request, jwtUtil: JWTUtil = Depends(),
+                         planService: PlanService = Depends(get_plan_service), userService: UserService = Depends()):
+    # jwt 토큰 분석
+    payload = userService.get_payload(request.cookies.get("jwt_token"), jwtUtil)
+
+    user_name: str = payload["username"]
+    plans = planService.get_my_plans(GetMyPlanListRequest(user_id=payload["id"]))
 
     return templates.TemplateResponse(
         request=request, name="plan.html", context={
             "plans": plans,
             "user": {
-                "user_id": user_id,
+                "user_id": payload["id"],
                 "user_name": user_name,
             }
         }
