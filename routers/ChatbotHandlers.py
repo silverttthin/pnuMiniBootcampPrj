80ef7edfa9b27428d73bb905bf26e2b17fcb5e9c
SonJH7 <sjunh02@naver.com>
2025-02-11 10:01:17 +0900

    save

--- routers/ChatbotHandlers.py
+++ routers/ChatbotHandlers.py
@@ -1,16 +1,4 @@
-﻿# from fastapi import APIRouter, Depends
-# from models.chatbot import Chatbot
-
-# router = APIRouter(prefix="/v1/chat")
-
-# @router.post("/message")
-# async def chat(user_input: str, chatbot=Depends(Chatbot)):
-#     chatbot.add_user_message(user_input)
-#     response = chatbot.send_request()
-#     chatbot.add_response(response)
-#     return {"response": chatbot.get_response()}
-
-from fastapi import APIRouter, Depends, Form
+﻿from fastapi import APIRouter, Depends, Form
 from models.chatbot import Chatbot
 
 router = APIRouter(prefix="/v1/chat")
