68e72b605b1f9deae762c285a19e71a937d68e52
Lee Si Woong <silverttthin@naver.com>
2025-02-12 10:38:44 +0900

    feat: 계획 삭제 버튼 구현

--- routers/PlanController.py
+++ routers/PlanController.py
@@ -69,8 +69,7 @@ async def delete_plan(req: PlanDeleteRequest, planService: PlanService = Depends
 
 # 내가 작성한 게획 리스트 가져오기
 @router.post("/list")
-async def get_my_plans(req: GetMyPlanListRequest, userService: UserService = Depends(),
-                       planService: PlanService = Depends(get_plan_service)):
+async def get_my_plans(req: GetMyPlanListRequest, planService: PlanService = Depends(get_plan_service)):
     plans = planService.get_my_plans(req)
     return plans
 
